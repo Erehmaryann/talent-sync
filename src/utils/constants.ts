eef5151509a4771f0f1557e84e6ff718a5742b31
Erehmaryann <edwardmaryann09@gmail.com>
2024-02-05 20:30:08 +0100

    first commit

---
+++ src/utils/constants.ts
@@ -0,0 +1,136 @@
+import FirstAvatar from "@/assets/icons/first-avatar.png";
+import SecondAvatar from "@/assets/icons/second-avatar.png";
+import ThirdAvatar from "@/assets/icons/third-avatar.png";
+import FourthAvatar from "@/assets/icons/fourth-avatar.png";
+import FifthAvatar from "@/assets/icons/fifth-avatar.png";
+
+import Shopify from "@/assets/icons/shopify.png";
+import Coinbase from "@/assets/icons/coinbase.png";
+import Dropbox from "@/assets/icons/dropbox.png";
+import Intercom from "@/assets/icons/intercom.png";
+import Marvel from "@/assets/icons/marvel.png";
+import Automatic from "@/assets/icons/automatic.png";
+
+import CrystalClear from "@/assets/icons/crystal-clear.png";
+import Audio from "@/assets/icons/audio.png";
+import Schedule from "@/assets/icons/schedule.png";
+import BankGrade from "@/assets/icons/bank-grade.png";
+
+export const navItems = [
+  {
+    id: 1,
+    text: "Products",
+    href: "/",
+  },
+  {
+    id: 2,
+    text: "Solutions",
+    href: "/",
+  },
+  {
+    id: 3,
+    text: "Resources",
+    href: "/",
+  },
+  {
+    id: 4,
+    text: "Pricing",
+    href: "/",
+  },
+];
+
+export const reviewsImg = [
+  { id: 1, src: FirstAvatar },
+  { id: 2, src: SecondAvatar },
+  { id: 3, src: ThirdAvatar },
+  { id: 4, src: FourthAvatar },
+  { id: 5, src: FifthAvatar },
+];
+
+export const partnersLogo = [
+  {
+    id: 1,
+    src: Shopify,
+  },
+  {
+    id: 2,
+    src: Coinbase,
+  },
+  {
+    id: 3,
+    src: Dropbox,
+  },
+  {
+    id: 4,
+    src: Intercom,
+  },
+  {
+    id: 5,
+    src: Marvel,
+  },
+  {
+    id: 6,
+    src: Automatic,
+  },
+];
+
+export const whyChooseUsData = [
+  {
+    id: 1,
+    src: CrystalClear,
+    heading: "Crystal-clear HD video",
+    desc: "No more pixelation or blurriness – just stunning, lifelike clarity that brings your team closer in meetings.",
+  },
+  {
+    id: 2,
+    src: Audio,
+    heading: "Noise-canceling audio",
+    desc: "Say goodbye to distractions with our advanced audio tech for crisp, interruption-free conversations.",
+  },
+  {
+    id: 3,
+    src: Schedule,
+    heading: "Scheduling made easy",
+    desc: "Streamline your agenda with ClearLink's intuitive scheduling. Set up meetings, send invitations, and receive reminders in one place.",
+  },
+  {
+    id: 4,
+    src: BankGrade,
+    heading: "Bank-grade security",
+    desc: "Your privacy is our priority with bank-grade security protocols safeguarding your meetings and data from unwanted intruders.",
+  },
+];
+
+export const faqData = [
+  {
+    id: 1,
+    question: "How many participants can join a ClearLink video conference?",
+    answer: `ClearLink offers flexible meeting options. Depending on your subscription plan, you can host meetings with varying numbers of participants. Our plans are designed to accommodate small team collaborations and large-scale webinars, ensuring you have the right fit for your needs.`,
+  },
+  {
+    id: 2,
+    question: "Can I use ClearLink on multiple devices?",
+    answer: `Yes Of course you can.`,
+  },
+  {
+    id: 3,
+    question:
+      "Is ClearLink compatible with other video conferencing platforms?",
+    answer: `Yes, it is.`,
+  },
+  {
+    id: 4,
+    question: "How does ClearLink ensure the security of my video conferences?",
+    answer: "By making sure it's encrypted",
+  },
+  {
+    id: 5,
+    question: "Do I need to download any software to use ClearLink?",
+    answer: `No, you don't have to`,
+  },
+  {
+    id: 6,
+    question: "What kind of customer support does ClearLink provide?",
+    answer: "Check our Docs for more insight",
+  },
+];
